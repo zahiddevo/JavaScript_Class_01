bf859bd81b9ae8333024001352a4da318eb08f5d
zahiddevo <zahid.h.redoy@gmail.com>
2025-09-21 15:02:33 +0600

    add

---
+++ Home_Work/script.js
@@ -0,0 +1,104 @@
+//=======Variable and Condition=====>
+
+//Frist Condition
+
+let learn = ('javaScritpt');
+learning = ('JavaScriptNo');
+
+//greater > 
+
+if (learn > learning) {
+    console.log("i will good for javascript");
+}else {
+    console.log("i will not good at javascript");
+}
+
+ //Less <
+
+if (learn < learning) {
+    console.log("i will good for javascript");
+}else {
+    console.log("i will not good at javascript");
+}
+
+let x = 560;
+y = 440;
+
+ //Less <
+
+if (x < y) {
+    console.log("Ami JavaScript a valo korbo");
+}else {
+    console.log("Ami JavaScript a valo korbo na");
+}
+
+//greater >
+
+if (x > y) {
+    console.log("Ami JavaScript a valo korbo");
+}else {
+    console.log("Ami JavaScript a valo korbo na");
+}
+
+//Second Condition
+
+let money = 350;
+fryChiken = 165;
+
+//greater > and equal
+if (money >= 350) {
+    console.log("I am going to watch a Horror Movie Conjuring")
+
+//less < and equal
+
+ if (fryChiken <= 150) {
+    console.log("Buying two fryChiken");
+}else {
+    console.log("fryChiken is so Expensive");
+} 
+} else{
+    console.log("I am wathing the another movie in my home alone")
+}
+
+//const, let, var
+
+//var
+
+var number = 10;
+var namber = 20;
+console.log(namber + number);
+console.log(namber - number);
+console.log(namber * number);
+
+//let
+
+let egg = "10 dojon";
+let potato = "5 kg";
+let onion = "3 kg";
+let red_chilli = "250g";
+let cardemon = "150g";
+console.log(egg);
+console.log(potato);
+console.log(onion);
+console.log(red_chilli);
+console.log(cardemon);
+
+// const
+
+const hi =("Md Zahid Hossin Redoy");
+const hello = ("I am Md Hossain");
+const whatAboutYou = ("I am fine thank you");
+const whereDoYouLive = ("I live in Dhaka Bangladesh");
+const niceToMeetYou = ("Nice to meet you too");
+console.log(hi); 
+console.log(hello); 
+console.log(whatAboutYou); 
+console.log(whereDoYouLive); 
+console.log(niceToMeetYou); 
+
+
+
+
+
+
+
